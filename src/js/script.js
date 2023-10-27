36741b909841a944c506af7ad02155b8b31048d9
Irina Trofimenko <politova_i@mail.ru>
2023-10-28 02:48:21 +0500

    Добавит логику для модалки, смены темы и подключения компонентов

---
+++ src/js/script.js
@@ -0,0 +1,39 @@
+// Модалка формы
+const modalForm = new Modal('#modal', '#signup')
+// Модалка навигации в мобилке
+const modalNav = new Modal('#modal-nav', '.burger-menu')
+
+/* Слайдер в баннере */
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  direction: 'horizontal',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  // breakpoints: {
+  //     545: {},
+  // },
+})
+
+const inputText = document.querySelector('#register-username')
+
+/* Обработчик событий текстовый инпут */
+inputText.addEventListener('change', (event) => {
+  const error = document.querySelector('#username-error')
+
+  // Регулярное выражение для проверки имени пользователя
+  const usernamePattern = /^[a-zA-Zа-яА-Я]{3,}$/
+
+  if (!event.target.value) {
+    error.textContent = ''
+  } else if (!usernamePattern.test(event.target.value)) {
+    error.textContent = 'Имя пользователя не соответствует требованиям'
+  } else {
+    error.textContent = ''
+  }
+})
